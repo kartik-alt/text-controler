e9699a3a0b575fc809d57754f76baffaa8d3c3ef
kartik-alt <73581266+kartik-alt@users.noreply.github.com>
2021-06-23 16:44:00 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+nosex=0;
+nosey=0;
+rightwristx=0;
+leftwristx=0;
+diffrence=0;
 function setup(){
 canvas=createCanvas(500,500);
 canvas.position(800,150);
@@ -12,12 +17,21 @@ pn.on('pose',gotPoses);
 
 function draw(){
     background("#969A97");
-
+    fill("red")
+    text("kartik",nosex, nosey);
+    textSize(diffrence);
 }
 
 function gotPoses(results){
 if(results.length>0){
 console.log(results);
+nosex=results[0].pose.nose.x;
+      nosey=results[0].pose.nose.y;
+      console.log("nosex= "+nosex+"nosey= "+nosey);
+      rightwristx= results[0].pose.rightWrist.x;
+      leftwristx= results[0].pose.leftWrist.x;
+      diffrence= floor(leftwristx-rightwristx);
+      console.log(" leftwrist= "+leftwristx+"rightwristx= "+rightwristx+"diffrence= "+diffrence);
 }
 
 }
